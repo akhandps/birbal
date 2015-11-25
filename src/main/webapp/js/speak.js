d35fa5437b31f1e470bbf0492ff3c069aa7e9f90
Akhand Pratap Singh <akhands@amazon.com>
2015-11-26 03:30:58 +0530

    chunky

--- src/main/webapp/js/speak.js
+++ src/main/webapp/js/speak.js
@@ -2,21 +2,70 @@
 function speak(text, callback) {
     var u = new SpeechSynthesisUtterance();
     u.text = text;
-    u.lang = 'en-US';
+    u.lang = 'hi-IN';
+    u.voice = speechSynthesis.getVoices()[7];
     Bot.listenStop();
-    u.onend = function () {
-      Bot.listenStart();
-        if (callback) {
+   
+    speechUtteranceChunker(u, {
+    chunkLength: 120
+      }, function () {
+    Bot.listenStart();
+    if (callback) {
             callback();
         }
-    };
- 
-    u.onerror = function (e) {
-        Bot.listenStart();
-        if (callback) {
-            callback(e);
-        }
-    };
- 
-    speechSynthesis.speak(u);
+    });
 }
+
+var speechUtteranceChunker = function (utt, settings, callback) {
+    settings = settings || {};
+    var newUtt;
+    var txt = (settings && settings.offset !== undefined ? utt.text.substring(settings.offset) : utt.text);
+    if (utt.voice && utt.voice.voiceURI === 'native') { // Not part of the spec
+        newUtt = utt;
+        newUtt.text = txt;
+        newUtt.addEventListener('end', function () {
+            if (speechUtteranceChunker.cancel) {
+                speechUtteranceChunker.cancel = false;
+            }
+            if (callback !== undefined) {
+                callback();
+            }
+        });
+    }
+    else {
+        var chunkLength = (settings && settings.chunkLength) || 160;
+        var pattRegex = new RegExp('^[\\s\\S]{' + Math.floor(chunkLength / 2) + ',' + chunkLength + '}[.!?,]{1}|^[\\s\\S]{1,' + chunkLength + '}$|^[\\s\\S]{1,' + chunkLength + '} ');
+        var chunkArr = txt.match(pattRegex);
+
+        if (chunkArr[0] === undefined || chunkArr[0].length <= 2) {
+            //call once all text has been spoken...
+            if (callback !== undefined) {
+                callback();
+            }
+            return;
+        }
+        var chunk = chunkArr[0];
+        newUtt = new SpeechSynthesisUtterance(chunk);
+        newUtt.voice = utt.voice;
+        newUtt.lang = utt.lang;
+
+        newUtt.addEventListener('end', function () {
+            if (speechUtteranceChunker.cancel) {
+                speechUtteranceChunker.cancel = false;
+                return;
+            }
+            settings.offset = settings.offset || 0;
+            settings.offset += chunk.length - 1;
+            speechUtteranceChunker(utt, settings, callback);
+        });
+    }
+
+    if (settings.modifier) {
+        settings.modifier(newUtt);
+    }
+    console.log(newUtt); //IMPORTANT!! Do not remove: Logging the object out fixes some onend firing issues.
+    //placing the speak invocation inside a callback fixes ordering and onend issues.
+    setTimeout(function () {
+        speechSynthesis.speak(newUtt);
+    }, 0);
+};
